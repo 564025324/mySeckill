d4bc7d07e7ceb37eb2a2d022c967d32b8679fd00
564025324 <564025324@qq.com>
2018-05-25 17:53:03 +0800

    创建数据库连接方式： 	1.引入commons-dbcp.jar 和commons-pool.jar 到项目；使用其中的数据库驱动Driverclass ； 	2.在xml配置文件中将对应<bean>中加入<property id="dataSource" ref="dataSource"/>； 	3.通过 this.getJdbcTemples()获取连接数据库的对象。 	--------------------- 	以上纯属胡编乱造，具体详见：https://blog.csdn.net/wuruiaoxue/article/details/54006772

---
+++ src/com/lijwen/util/TestJdbc.java
@@ -0,0 +1,36 @@
+package com.lijwen.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import com.lijwen.dao.ISeckillDao;
+import com.lijwen.entity.Seckill;
+
+@SuppressWarnings("deprecation")
+public class TestJdbc {
+	public static void main(String[] args) {
+		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("spring/spring-dao.xml"));
+		ISeckillDao lldao = (ISeckillDao) factory.getBean("ljwdao");
+
+		String name = lldao.getSeckillName(1004);
+
+		System.out.println(" name : " + name);
+
+		// SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		// Seckill sk = new Seckill();
+		// try {
+		// sk.setName("测试数据-1元秒杀牧马人");
+		// sk.setNumber(Integer.valueOf("1"));
+		// sk.setStarttime(sdf.parse("2018-05-25 11:00:00"));
+		// sk.setEndtime(sdf.parse("2018-05-25 11:05:00"));
+		// System.out.println(sk);
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
+		// lldao.addSeckill(sk);
+
+	}
+}
