b9239bae15c0207f67429418906c6e4c7fa6de27
564025324 <564025324@qq.com>
2018-06-01 02:00:49 +0800

    调整 Quartz 定时任务时间和状态以及执行方法内容。

--- src/com/lijwen/service/TestQuartzService.java
+++ src/com/lijwen/service/TestQuartzService.java
@@ -8,6 +8,6 @@ public class TestQuartzService {
 	public void printHelloMethod() {
 		Date now = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println(sdf.format(now));
+		System.out.println("定时任务打印当前时间 ： " + sdf.format(now));
 	}
 }
